f97f7fba3cf8ef379e0dd4825b81e25eba9ddb7f
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-05-30 21:04:22 +0900

    complete title banner

--- src/comp/Banner.tsx
+++ src/comp/Banner.tsx
@@ -3,19 +3,40 @@ import "../styles/Banner.css";
 
 const Banner: React.FC = () => {
   
-  const lines: string[] = [
-    "劇場で野球をしては",
-    "いけません！！！"
+  const inWaitingRoomBanner: string[] = [
+    "控え室で",
+    "野球をしては",
+    "いけません！"
   ];
+
+  const inDressRoomBanner: string[] = [
+    "ここで",
+    "野球をしては",
+    "いけません！！"
+  ];
+
+  const inStretchRoomBanner: string[] = [
+    "ここで",
+    "野球ダメ",
+    "ゼッタイ！"
+  ]
+
+  const bannerVariations: string[][] = [
+    inWaitingRoomBanner,
+    inDressRoomBanner,
+    inStretchRoomBanner
+  ];
+
+  const lines: string[] = bannerVariations[Math.floor(Math.random() * bannerVariations.length)];
 
   return (
     <>
       <div className="banner">
         {lines.map((line: string, i: number) => {
           return (
-            <div key={i} className="banner-line">
-              {line}
-            </div>
+            <p key={i} className="banner-line">
+              {line}<br />
+            </p>
           );
         })}
       </div>
