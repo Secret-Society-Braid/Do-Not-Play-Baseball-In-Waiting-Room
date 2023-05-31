a395a11edbfe3f8c9b78f3b5c10d95bd9ce3a7c5
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-05-31 22:05:57 +0900

    Lines are fixed (#4)

--- src/comp/UserInputs.tsx
+++ src/comp/UserInputs.tsx
@@ -1,40 +1,24 @@
 import "../styles/userInputs.css";
-import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement, reset } from "../features/lineCounterSlice";
 
 const UserInputs = () => {
 
-  const lineCount = useSelector((state: any) => state.lineCounter.count);
-  const dispatch = useDispatch();
-
   return (
     <>
-      <div className="line-counter">
-        <input
-          type="button"
-          value="+"
-          onClick={() => dispatch(increment())}
-        />
-        <h2>{lineCount}</h2>
-        <input
-          type="button"
-          value="-"
-          onClick={() => dispatch(decrement())}
-        />
-      </div>
       <div className="line-inputs">
-        {[...Array(lineCount)].map((_, i) => (
-          <input
-            type="text"
-            key={i}
-            placeholder={`${i + 1} 行目`}
-            className={"line-input"}
-          />
-        ))}
+        {[...Array(3)].map((_, i: number) => {
+          return (
+              <input
+                type="text"
+                key={i}
+                placeholder={`${i + 1}行目`}
+                className="line-input"
+                onChange={() => {}}
+              />
+          );
+        })}
       </div>
       <div className="submit">
         <input type="submit" value="貼り紙をつくる" onClick={() => {}}/>
-        <input type="submit" value="行数をリセット" onClick={() => dispatch(reset())}/>
       </div>
       <div className="result">
       </div>
